e10805a7c8980d188be30dfa73a7beec7dc3de2a
EssEnemiGz <75174446+EssEnemiGz@users.noreply.github.com>
2021-08-19 21:18:56 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -8,15 +8,19 @@ except ModuleNotFoundError:
     os.system('pip install lxml')
 finally:
     print("Iniciando ejecución")
+
+from bs4 import BeautifulSoup                                
+import requests
+
 # El código se arriba es solo para evitar errores
 
 # import lxml
 
-r = input("Página a buscar (URL de la página, hasta ahora solo steam): ") # Intermediario para buscar la página deseada 
-s = requests.get(r) # Request va a almacenar el HTML de la página
-interaction = BeautifulSoup(s.content, "lxml") # Interacción con los elementoa del html
+# r = input("Página a buscar (URL de la página, hasta ahora solo steam): ") # Intermediario para buscar la página deseada 
+s = requests.get("https://store.steampowered.com/app/1091500/Cyberpunk_2077/") # Request va a almacenar el HTML de la página
+soup = BeautifulSoup(s.content, "lxml") # Interacción con los elementoa del html
 
-n = soup.find('div', id="appHubAppName").txt # Nombre del juego de steam 
+n = soup.find('div', id="apphub_AppName").txt # Nombre del juego de steam 
 
 i = soup.find("El precio de",n,"es: ","div", class_="game_purchase_price price").txt # Precio de steam en texto
 
